9bb7ce980d8a25beccf1022192a0240bbd4130fd
GeoffrayCM <muyombe@hotmail.fr>
2025-10-22 11:01:50 +0000

    4-print_alphabt.c

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -13,10 +13,10 @@ int main(void)
 
 	while (alphabet <= 'z')
 	{
-		if (alphabet != 'e' && alphabet != 'q')	
+		if (alphabet != 'e' && alphabet != 'q')
 			putchar(alphabet);
 		alphabet++;
 	}
 	putchar('\n');
 	return (0);
-} 
+}
