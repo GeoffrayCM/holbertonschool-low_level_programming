108852c075688aacabd3ad4e88c61e8c86f19a6c
GeoffrayCM <muyombe@hotmail.fr>
2025-10-22 10:17:13 +0000

    3-print_alphabets.c

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -5,18 +5,20 @@
 /**
  * main - Prints the alphabet
  *
- * return: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	char alphabet = 'a';
+	char maj = 'A';
 
 	while (alphabet <= 'z')
 	{
 		putchar(alphabet);
 		alphabet++;
 	}
+	while (maj <= 'Z')
+		putchar(maj++);
 	putchar('\n');
-
 	return (0);
 }
