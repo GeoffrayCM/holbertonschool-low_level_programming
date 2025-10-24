26cd7c2f378cd4bed136c375fdd81568624b7f80
GeoffrayCM <muyombe@hotmail.fr>
2025-10-24 10:28:59 +0000

    main.h

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,13 +1,3 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-/**
- * File: main.h
- * Author: GeoffrayCM
- * Description: En-tête contennt les prototypes des fonctions
- * utilisées dans le projt functions_nested_loops.
- */
-
+#include <unistd.h>
+void print_alphabet(void);
 int _putchar(char c);
-
-#endif /* MAIN_H */
