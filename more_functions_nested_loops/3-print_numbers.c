240cb715c9d62636c6b4449e9dbe94621615501f
GeoffrayCM <muyombe@hotmail.fr>
2025-10-28 14:02:59 +0000

    3-print_numbers.c

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_numbers - Entry point
+ *
+ * Description: prints the numbers, from 0 to 9, followed by a new line
+ * Return: Always 0 (Success)
+ */
+void print_numbers(void)
+{
+	int c = 0;
+
+	while (c <= 9)
+	{
+		putchar(c + '0');
+		c++;
+	}
+	putchar('\n');
+}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,6 +1,4 @@
-#ifndef MAIN_H
-#define MAIN_H
-
+#include<unistd.h>
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
@@ -12,5 +10,3 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
-
-#endif /*MAIN_H*/
