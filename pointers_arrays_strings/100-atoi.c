183dd28c8f1bb23dd209b51ba373b400d8c2a223
GeoffrayCM <muyombe@hotmail.fr>
2025-10-31 15:01:45 +0000

    100-atoi.c

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _atoi - convertit une chaîne en entier
+ * @s: pointeur vers la chaîne
+ *
+ * Return: entier converti, ou 0 si aucun chiffre trouvé
+ */
+int _atoi(char *s)
+{
+	int i = 0, sign = 1, num = 0;
+	int found_digit = 0;
+
+	/* Parcourir les caractères */
+	while (s[i] != '\0')
+	{
+		if (s[i] == '-')
+			sign *= -1;
+		else if (s[i] >= '0' && s[i] <= '9')
+		{
+			found_digit = 1;
+			num = num * 10 + (s[i] - '0');
+		}
+		else if (found_digit)
+			break;
+		i++;
+	}
+
+	return (num * sign);
+}
+
