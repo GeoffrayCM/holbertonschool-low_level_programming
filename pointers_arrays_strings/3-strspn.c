490145af9669c12ef8b216466f89fabae275ec7d
GeoffrayCM <muyombe@hotmail.fr>
2025-11-05 11:29:35 +0000

    3-strspn.c

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -1,27 +1,27 @@
 #include "main.h"
 
-/** 
-* _strspn - n bytes from accept in s,
-* @s: main string,
-* @accept: bytes to find,
-* Return: lenght of bytes found
-*/
+/**
+ * _strspn - n bytes from accept in s,
+ * @s: main string,
+ * @accept: bytes to find,
+ * Return: lenght of bytes found
+ */
 
 unsigned int _strspn(char *s, char *accept)
 {
-    int i = 0;
-    int j = 0;
+	int i = 0;
+	int j = 0;
 
-    while (s[i])
-    {
-        j = 0;
-        while (accept[j] != s[i])
-        {
-            if (accept[j] == '\0')
-                return (i);
-            j++;
-        }
-        i++;
-    }
-    return (i);
+	while (s[i])
+	{
+		j = 0;
+		while (accept[j] != s[i])
+		{
+			if (accept[j] == '\0')
+				return (i);
+			j++;
+		}
+		i++;
+	}
+	return (i);
 }
